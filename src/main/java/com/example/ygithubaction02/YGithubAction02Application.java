8c6e9f0dae07d6964052572fbaf2d7669854c57c
ERLMKea <erlm@kea.dk>
2023-01-03 17:05:21 +0100

    only check out

---
+++ src/main/java/com/example/ygithubaction02/YGithubAction02Application.java
@@ -0,0 +1,13 @@
+package com.example.ygithubaction02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YGithubAction02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YGithubAction02Application.class, args);
+    }
+
+}
